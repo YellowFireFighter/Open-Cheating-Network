b8e3d8eebaecf56def0a808418c834d0789368ef
Yellow <106484528+YellowFireFighter@users.noreply.github.com>
2025-12-03 16:13:10 -0600

    Add IntegrationService for chat functionality
    Implement integration service for chat and user management

---
+++ Client/Example.lua
@@ -0,0 +1,52 @@
+local IntegrationService = loadstring(request({Url = "https://chronos.rip/static/inter.lua", Method = "Get"}).Body)()
+
+IntegrationService.Init({
+    serverUrl = "wss://chronos.rip/swimhub",
+    heartbeatInterval = 5,
+    autoReconnect = true,
+    hidden = false
+})
+
+IntegrationService.OnChatMessage.Event:Connect(function(username, message, timestamp)
+    print(string.format("[%s]: %s", username, message))
+end)
+
+IntegrationService.OnSystemMessage.Event:Connect(function(message, timestamp)
+    print("[SYSTEM]", message)
+end)
+
+IntegrationService.OnUserListUpdate.Event:Connect(function(users, timestamp)
+    print("[USERS]", table.concat(users, ", "))
+end)
+
+IntegrationService.OnConnected.Event:Connect(function(username, token, hidden)
+    print("Connected as:", username)
+    print("Auth token:", token)
+    print("Hidden mode:", hidden)
+end)
+
+IntegrationService.OnDisconnected.Event:Connect(function()
+    print("Disconnected from server")
+end)
+
+IntegrationService.OnError.Event:Connect(function(errorMessage, timestamp)
+    warn("Server error:", errorMessage)
+end)
+
+IntegrationService.SendMessage("Hello everyone!")
+
+IntegrationService.GetUsers()
+
+if IntegrationService.IsConnected() then
+    print("Connected!")
+end
+
+if IntegrationService.IsHidden() then
+    print("Running in hidden mode")
+end
+
+IntegrationService.SetHidden(true)
+
+IntegrationService.SetHidden(false)
+
+IntegrationService.Disconnect()
