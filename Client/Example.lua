283f385d9f338e4fd62dda0a214f6013a0cd9c5c
Yellow <jonathanstandley32@gmail.com>
2025-12-04 22:13:55 -0600

    Fixed initilizing b4 connections wont fire them, added exception handler to sending messages (usually errors when disconnecting while trying to send a message to a user)

--- Client/Example.lua
+++ Client/Example.lua
@@ -1,12 +1,5 @@
 local IntegrationService = loadstring(request({Url = "https://raw.githubusercontent.com/YellowFireFighter/Open-Cheating-Network/refs/heads/main/Client/Main.lua", Method = "Get"}).Body)()
 
-IntegrationService.Init({
-    serverUrl = "wss://localhost:8888/swimhub",
-    heartbeatInterval = 5,
-    autoReconnect = true,
-    hidden = false
-})
-
 IntegrationService.OnChatMessage.Event:Connect(function(username, message, timestamp)
     print(string.format("[%s]: %s", username, message))
 end)
@@ -33,10 +26,6 @@ IntegrationService.OnError.Event:Connect(function(errorMessage, timestamp)
     warn("Server error:", errorMessage)
 end)
 
-IntegrationService.SendMessage("Hello everyone!")
-
-IntegrationService.GetUsers()
-
 if IntegrationService.IsConnected() then
     print("Connected!")
 end
@@ -44,6 +33,18 @@ end
 if IntegrationService.IsHidden() then
     print("Running in hidden mode")
 end
+
+-- Init after everything is setup
+IntegrationService.Init({
+    serverUrl = "ws://localhost:8888/swimhub", -- ws for local host, wss for secure links
+    heartbeatInterval = 5,
+    autoReconnect = true,
+    hidden = false
+})
+
+IntegrationService.SendMessage("Hello everyone!")
+
+IntegrationService.GetUsers()
 
 IntegrationService.SetHidden(true)
 
